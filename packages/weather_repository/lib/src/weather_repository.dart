6b629bcfb994c55ea74a5a4d1a58ee618672e685
dhanazam <ramadanazam93@gmail.com>
2024-06-10 11:37:28 +0800

    wip WeatherRepository

---
+++ packages/weather_repository/lib/src/weather_repository.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:open_meteo_api/open_meteo_api.dart' hide Weather;
+import 'package:weather_repository/weather_repository.dart';
+
+class WeatherRepository {
+  WeatherRepository({OpenMeteoApiClient? weatherApiClient}) : _weatherApiClient = weatherApiClient ?? OpenMeteoApiClient();
+
+  final OpenMeteoApiClient _weatherApiClient;
+
+  Future<Weather> getWeather(String city) async {
+    final location = await _weatherApiClient.locationSearch(city);
+    final weather = await _weatherApiClient.getWeather(latitude: location.latitude, longitude: location.longitude);
+
+    return Weather(temperature: weather.temperature, location: location.name, condition: weather.weatherCode.toInt().toCondition,);
+  }
+}
