925ce2b0e3a66c234ba9b81f301583eb620ff8dd
dhanazam <ramadanazam93@gmail.com>
2024-06-10 09:35:43 +0800

    wip test

---
+++ packages/open_meteo_api/test/open_meteo_api_client_test.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:http/http.dart' as http;
+import 'package:open_meteo_api/open_meteo_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('OpenMeteoApiClient', () {
+    late http.Client httpClient;
+    late OpenMeteoApiClient apiClient;
+
+    setUpAll(() {
+      
+    })
+  })
+}
