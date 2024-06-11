7cb3566750d16939f4e7ed9359b36ee918f2c09f
dhanazam <ramadanazam93@gmail.com>
2024-06-11 11:43:19 +0800

    fix status

--- lib/weather/cubit/weather_state.dart
+++ lib/weather/cubit/weather_state.dart
@@ -5,7 +5,7 @@ enum WeatherStatus { initial, loading, success, failure }
 extension WeatherStatusX on WeatherStatus {
   bool get isInitial => this == WeatherStatus.initial;
   bool get isLoading => this == WeatherStatus.loading;
-  bool get isSucccess => this == WeatherStatus.success;
+  bool get isSuccess => this == WeatherStatus.success;
   bool get isFailure => this == WeatherStatus.failure;
 }
 
