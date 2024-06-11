ca62ef97cb52c455d80fde4821d63a0fd06feaa9
dhanazam <ramadanazam93@gmail.com>
2024-06-11 20:41:50 +0800

    add setting page

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -17,7 +17,25 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(title: const Text('Settings')),
+      body: ListView(
+        children: <Widget>[
+          BlocBuilder<WeatherCubit, WeatherState>(
+            buildWhen: (previous, current) => previous.temperatureUnits != current.temperatureUnits,
+            builder: (context, state) {
+              return ListTile(
+                title: const Text('Temperature Units'),
+                isThreeLine: true,
+                subtitle: const Text('Use metric measurements for temperature units.'),
+                trailing: Switch(
+                  value: state.temperatureUnits.isCelsius,
+                  onChanged: (_) => context.read<WeatherCubit>().toggleUnits(),
+                )
+              );
+            },
+          )
+        ]
+      )
     );
   }
   
