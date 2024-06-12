cbdc918d4986767349330315f7976402e507ab10
dhanazam <ramadanazam93@gmail.com>
2024-06-12 10:54:22 +0800

    fix missing imports

--- lib/weather/view/weather_page.dart
+++ lib/weather/view/weather_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/search/view/search_page.dart';
+import 'package:flutter_weather/settings/settings_page.dart';
 import 'package:flutter_weather/weather/cubit/theme_cubit.dart';
 import 'package:flutter_weather/weather/weather.dart';
+import 'package:flutter_weather/weather/widgets/widget.dart';
 import 'package:weather_repository/weather_repository.dart';
 
 
